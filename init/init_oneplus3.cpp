9486c295f678a7b4fd15d6cb89bb6d3882d64e53
Martinusbe <martinusbe@gmail.com>
2017-01-02 12:02:22 +0100

    op3: fix and simplify unified ini
    We aren't shipping anything with the device yet
    Keep it simple and slim
    
    Change-Id: Ic0bb685d3e11a69c90517ad3d9499b4d396247b0
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -109,21 +109,12 @@ void vendor_load_properties() {
 
     property_get("ro.boot.rf_version", rf_version, NULL);
 
-    if (strstr(rf_version, "11") || strstr(rf_version, "31")) {
-        /* Chinese/America */
-        load_op3("ONEPLUS A3000");
-    } else if (strstr(rf_version, "21")) {
-        /* Asia/Europe */
-        load_op3("ONEPLUS A3003");
-    } else if (strstr(rf_version, "12")) {
-        /* America */
-        load_op3t("ONEPLUS A3000");
-    } else if (strstr(rf_version, "22")) {
-        /* Asia/Europe */
-        load_op3t("ONEPLUS A3003");
-    } else if (strstr(rf_version, "32")) {
-        /* Chinese */
-        load_op3t("ONEPLUS A3010");
+    if (strstr(rf_version, "11") || strstr(rf_version, "31") || strstr(rf_version, "21")) {
+        /* OnePlus3 */
+        load_op3("Slim OnePlus3");
+    } else if (strstr(rf_version, "12") || strstr(rf_version, "32") || strstr(rf_version, "22")) {
+        /* Oneplus3T */
+        load_op3t("Slim OnePlus3-T");
     }
 
     init_alarm_boot_properties();
