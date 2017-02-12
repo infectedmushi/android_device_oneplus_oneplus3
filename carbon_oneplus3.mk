f4703aea65b29fa8228bbcd04428c903979ffef4
infectedmushi <zitoz@netvisao.pt>
2017-02-12 06:13:37 +0000

    build kernel with linaro 4.9

--- carbon_oneplus3.mk
+++ carbon_oneplus3.mk
@@ -24,7 +24,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -33,13 +33,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 TARGET_BOOTANIMATION_SIZE := 1080x608
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_oneplus3
+PRODUCT_NAME := carbon_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
