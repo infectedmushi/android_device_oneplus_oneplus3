11792fd8877dbbde29b8c5e3f8f57c4a7b8fc43a
infectedmushi <infectedmushi@gmail.com>
2017-05-03 12:41:40 +0100

    build kernel with uber 4.9
    Change-Id: I7630af7dc623efedee4910fdfaf50eec8e3b0784

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
 TARGET_KERNEL_CONFIG := lineageos_oneplus3_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
