9b91ec232ecc94ef3730c9851184e972a199af5e
infectedmushi <infectedmushi@gmail.com>
2017-05-14 16:08:11 +0100

    build kernel with ubertc 4.9
    Change-Id: I23a9344cff8bf3259ea853130fa2d9749cc1d4e3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,10 @@ ENABLE_CPUSETS := true
 
 TARGET_USES_64_BIT_BINDER := true
 
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_BASE := 0x80000000
