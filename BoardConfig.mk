ed8c77e5d95b90f6c49cad7c34b2c4bdfec0d46a
infectedmushi <infectedmushi@gmail.com>
2017-07-19 06:38:19 +0100

    build kernel with ubertc
    Change-Id: Ic9d9313b484829d0f09865569c15d0fddd45b59e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,9 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
 TARGET_KERNEL_CONFIG := oneplus3_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
