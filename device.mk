c8dd69709cfed5678f223209afa658a17d37654d
dianlujitao <dianlujitao@lineageos.org>
2017-08-15 18:04:22 +0800

    op3: Added Sustained and VR perf mode powerhints in xml
    Sustained performance mode:
    All CPUs are capped to ~1.2GHz
    GPU frequency is capped to 315MHz
    
    VR+Sustained performance mode:
    All CPUs are locked to ~1.2GHz
    GPU frequency is locked to 315MHz
    GPU BUS min_freq is raised to 790MHz
    
    VR mode:
    All CPUs are locked to ~1.44GHz
    GPU frequency is locked to 510MHz
    GPU BUS min_freq is raised to 790MHz
    
    Change-Id: I569e7861c675c4557c08d488a1def46240802714
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- device.mk
+++ device.mk
@@ -281,6 +281,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.msm8996
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
